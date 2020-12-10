7aec19ca9e9a7d975f7b984c913e7b7fc09c2d5e
Paulo ES <pauloeduardo.dg@outlook.com>
2020-12-09 22:53:10 -0300

    Back-end App
    Classes que controlam a aplicação.
    Criar a pasta "app_backend" fora do diretório público da sua hospedagem.
    Incluir arquivos na pasta.

---
+++ tarefa.service.php
@@ -0,0 +1,21 @@
+<?php
+
+//CRUD
+class TarefaService{
+    public function inserir() { //create
+
+    }
+
+    public function recuperar() { //read
+
+    }
+
+    public function atualizar() { //update
+
+    }
+
+    public function remover() { //delete
+
+    }
+
+}
