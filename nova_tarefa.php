e47096c5b2ee6cb0b8ef6a718362d82cef89cec9
pesdesigner <pauloeduardo.dg@outlook.com>
2020-12-11 23:54:17 -0300

    Listando todos os registros

--- nova_tarefa.php
+++ nova_tarefa.php
@@ -18,11 +18,11 @@
 				</a>
 			</div>
 		</nav>
-		<? if( isset($_GET['inclusao']) && $_GET['inclusao'] == 1) { ?> 
+		<?php if(isset($_GET['inclusao']) && $_GET['inclusao'] == 1) { ?> 
 		<div class="bg-success pt-2 text-white d-flex justify-content-center">
 			<h5>Tarefa inserida com sucesso!</h5>
 		</div>
-		<? } ?>
+		<?php } ?>
 		<div class="container app">
 			<div class="row">
 				<div class="col-md-3 menu">
@@ -40,7 +40,7 @@
 								<h4>Nova tarefa</h4>
 								<hr />
 
-								<form method="POST" action="tarefa_controller.php">
+								<form method="POST" action="tarefa_controller.php?acao=inserir">
 									<div class="form-group">
 										<label>Descrição da tarefa:</label>
 										<input type="text" class="form-control" name="tarefa" placeholder="Exemplo: Lavar o carro">
