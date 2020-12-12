97a2e2610c4e664febf5de3a66fb90e87034e8c2
Paulo ES <pauloeduardo.dg@outlook.com>
2020-12-11 22:32:45 -0300

    Inserindo registros de tarefas
    Enviando registros para o banco de dados

--- tarefa_controller.php
+++ tarefa_controller.php
@@ -1,3 +1,16 @@
 <?php
-require_once '../../app_backend/tarefa_controller.php';
+require "../../app_backend/tarefa.model.php";
+require "../../app_backend/tarefa.service.php";
+require "../../app_backend/conexao.php";
+
+$tarefa = new Tarefa();
+$tarefa->__set('tarefa', $_POST['tarefa']);
+
+$conexao = new Conexao();
+
+$tarefaService = new TarefaService($conexao, $tarefa);
+$tarefaService->inserir();
+
+header('Location: nova_tarefa.php?inclusao=1');
+
 ?>
